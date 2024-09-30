86f2128a72fdeb9681f02e6b45a9ac78aad2249f
rmo44 <rmo44@txstate.edu>
2024-09-30 18:22:04 -0500

    Add files via upload

---
+++ Intro.py
@@ -0,0 +1,136 @@
+
+#### Variables in python ####
+
+
+#Writing Variables pt.1#
+#x = 5
+#y = "John"
+#print(x)
+#print(y)
+
+#Writing Variables pt.2#
+#x = 4       
+#x = "Sally" 
+#print(x)
+
+#Writing Variables pt.3#
+#x = "Sally"  
+#x = 'Sally' 
+#print(x)
+
+#Casting# 
+#x = str(3)    
+#y = int(3)    
+#z = float(3)  
+#print(x, y, z)
+
+#Writing Variables pt.1#
+#x = 5
+#y = "John"
+#print(type(x))
+#print(type(y))
+
+#Case Sensitive Variable#
+#a = 4
+#A = "Sally"
+#print(???)
+
+###!!! BE CAREFUL WRITING YOUR VARIABLE NAMES SOME DON'T WORK!!!###
+
+#Assiging Variables pt.1#
+#x, y, z = "Roses", "Lilies", "Daisies"
+#print(x)
+#print(y)
+#print(z)
+
+#Assiging Variables pt.2#
+#x = y = z = "Daisy"
+#print(x)
+#print(y)
+#print(z)
+
+#Assiging Variables pt.3#
+#fruits = ["apple", "banana", "cherry"]
+#x, y, z = fruits
+#print(x)
+#print(y)
+#print(z)
+
+#Output Variables pt.1#
+#x = 5
+#y = 10
+#print(x + y)
+
+#Output Variables pt.2# !space mistake
+#x = "Want to "
+#y = "see something "
+#z = "cool?"
+#print(x, y, z)
+
+### How to write a function ###
+# def my_function():
+#   print("Hello from a function")
+
+# my_function()
+
+#Global Variables pt.1#
+#x = "awesome"
+#
+#def myfunc():
+#  print("Python is " + x)
+
+#myfunc()
+
+#Global Variables pt.2#
+# x = "awesome"
+
+# def myfunc():
+#  x = "fantastic"
+#  print("Python is " + x)
+
+# myfunc()
+
+# print("Python is " + x)
+
+#If 
+# a = 33
+# b = 200
+# if b > a:
+#   print("b is greater than a")
+
+#IF
+# if a > b: print("a is greater than b")
+
+# Else if 
+# a = 33
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+
+#Else
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+#   print("a is greater than b")
+
+#Else
+# a = 2
+# b = 330
+# print("A") if a > b else print("B")
+
+
+# Nested If Else
+# x = 41
+
+# if x > 10:
+#   print("Above ten,")
+#   if x > 20:
+#     print("and also above 20!")
+#   else:
+#     print("but not above 20.")
